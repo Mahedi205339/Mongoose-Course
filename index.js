1226e262307b82948d6d0a61c49bc83b771a4e66
Mahedi205339 <mahedi205339@gmail.com>
2024-08-10 14:25:21 +0600

    fixed all problems

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-// const mongoose = require("mongoose");
-// const User = require("./Users");
-
-// mongoose.connect("mongodb://localhost/testdb")
-
-
-// run()
-// async function run() {
-//     const user = new User({ name: " kyle", age: 23 })
-//     await user.save()
-//     console.log(user);
-// }
-
 const mongoose = require("mongoose");
 const User = require("./Users");
 
 mongoose.connect("mongodb://localhost/testdb")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Could not connect to MongoDB...", err));
 
-run().catch(err => console.error("Error:", err));
 
+run()
 async function run() {
-    const user = new User({ name: "Kyle", age: 23 });
-    await user.save();
+    const user = new User({
+        name: " kyle",
+        age: 23,
+        hobbies: ['Fishing', "Traveling", "Soccer"],
+        address:{
+            street:"lane 2",
+            city: "Dhaka"
+        }
+    })
+    user.name = "Sally"
+    await user.save()
     console.log(user);
 }
-
 

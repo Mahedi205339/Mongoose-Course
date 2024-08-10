1368610740254dd2767853fba41e8f96ec5d2e77
Mahedi205339 <mahedi205339@gmail.com>
2024-08-10 13:22:15 +0600

    continued bug fixig

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+// const mongoose = require("mongoose");
+// const User = require("./Users");
+
+// mongoose.connect("mongodb://localhost/testdb")
+
+
+// run()
+// async function run() {
+//     const user = new User({ name: " kyle", age: 23 })
+//     await user.save()
+//     console.log(user);
+// }
+
 const mongoose = require("mongoose");
 const User = require("./Users");
 
 mongoose.connect("mongodb://localhost/testdb")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Could not connect to MongoDB...", err));
 
+run().catch(err => console.error("Error:", err));
 
-run()
 async function run() {
-    const user = new User({ name: " kyle", age: 23 })
-    await user.save()
+    const user = new User({ name: "Kyle", age: 23 });
+    await user.save();
     console.log(user);
 }
+
 
